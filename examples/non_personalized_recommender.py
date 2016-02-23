15d70595a3cc16163ad3679061beed2b34c2781b
Dom Leone <ohheydom@gmail.com>
2016-02-23 16:38:52 -0500

    Fixed comments.

--- examples/non_personalized_recommender.py
+++ examples/non_personalized_recommender.py
@@ -18,20 +18,24 @@ all_books = brs.load_book_data('../book_data/BX-Books.csv')
 min_book_ratings = 4
 min_user_ratings = 3
 
-## Unfortunately, the amount of 0s in the dataset was heavily skewing the data. This removes all 0 values, which gives us about a third of the data to utilize
+# Unfortunately, the amount of 0s in the dataset was heavily skewing the data. This removes all 0 values, which gives us about a third of the data to utilize
 rated_books = rated_books[rated_books['Book-Rating'] != 0]
 
-### The following function keeps only the books with greater than min_book_ratings
+# The following function keeps only the books with greater than min_book_ratings
 rated_books = rated_books.groupby(rated_books.index).filter(lambda x: len(x) >= min_book_ratings)
 
-### The following function keeps only the users who rated min_user_ratings or greater books
+# The following function keeps only the users who rated min_user_ratings or greater books
 rated_books = rated_books.groupby(rated_books['User-ID']).filter(lambda x: len(x) >= min_user_ratings)
 
-# Non-Personalized Collaborative Filtering
-
-# Let's use the top rater and print out a list of the most popular books that he/she hasn't read yet.
+# Find the top 10 users with the most ratings 
 rater_list = rated_books['User-ID'].value_counts()[:10]
+
+# Find the highested rated books
 ncf = npcf.NonPersonalizedCF(rated_books, all_books)
+top_books = ncf.highest_rated_books(n=50, min_rating=8, max_rating=10)
+
+# Select the user with the most ratings
 user = rated_books[rated_books['User-ID'] == np.asarray(rater_list.axes).tolist()[0][0]]
-top_books = ncf.highest_rated_books(n=1500)
+
+# Print the top books that the user hasn't rated
 print ncf.recommend_books(user, top_books)
--- examples/personalized_k_fold.py
+++ examples/personalized_k_fold.py
@@ -17,19 +17,17 @@ rated_books = brs.load_rating_data('../book_data/BX-Book-Ratings.csv')
 min_book_ratings = 4
 min_user_ratings = 3
 
-## Unfortunately, the amount of 0s in the dataset was heavily skewing the data. This removes all 0 values, which gives us about a third of the data to utilize
+# Unfortunately, the amount of 0s in the dataset was heavily skewing the data. This removes all 0 values, which gives us about a third of the data to utilize
 rated_books = rated_books[rated_books['Book-Rating'] != 0]
 
-### The following function keeps only the books with greater than min_book_ratings
+# The following function keeps only the books with greater than min_book_ratings
 rated_books = rated_books.groupby(rated_books.index).filter(lambda x: len(x) >= min_book_ratings)
 
-### The following function keeps only the users who rated min_user_ratings or greater books
+# The following function keeps only the users who rated min_user_ratings or greater books
 rated_books = rated_books.groupby(rated_books['User-ID']).filter(lambda x: len(x) >= min_user_ratings)
 
-
-# Personalized Collaborative Filtering
-
 # K-Fold Cross Validation
+
 n_folds = 10
 total_error = 0.0
 books_to_omit = 2
